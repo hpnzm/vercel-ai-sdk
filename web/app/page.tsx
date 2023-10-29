14f4b8d9e7e36bd74ae27f2e4870c72b1bba6371
CodeSandbox <support@codesandbox.io>
2023-10-29 15:01:11 +0000

    Sandbox snapshot commit

---
+++ web/app/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useChat } from "ai/react";
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  return (
+    <div>
+      {messages.map((m) => (
+        <div key={m.id} className="mb-4">
+          {m.role === "user" ? "User: " : "AI: "}
+          {m.content}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Say something...
+          <input
+            className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
+            value={input}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}
